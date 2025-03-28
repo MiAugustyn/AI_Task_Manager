d1632e00e5d7171f5d316b507bb1c30142d000d8
ZelhinJT <126654763+ZelhinJT@users.noreply.github.com>
2025-03-28 12:47:48 +0100

    Update config.py - fix key naming

--- app/config.py
+++ app/config.py
@@ -5,9 +5,9 @@ import os
 class Config:
     # Ustawienia trybu debug oraz klucz sesji (pobrane z zmiennych środowiskowych lub domyślne)
     DEBUG = os.environ.get('FLASK_DEBUG', False)
-    SECRET_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    SECRET_BUCKET = os.environ.get('AWS_S3_BUCKET')
-    SECRET_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_S3_BUCKET = os.environ.get('AWS_S3_BUCKET')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
     # Konfiguracja bazy danych – używamy SQLite, ale można podmienić na np. RDS (DATABASE_URL)
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///task_manager.db')
