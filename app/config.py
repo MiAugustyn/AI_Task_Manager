9624bd2cb6fc1203ed93ff81dd8724511a2f4980
zelhin <jakub.t.zelhin@gmail.com>
2025-03-28 12:15:50 +0100

    Added secret keys in config.py

--- app/config.py
+++ app/config.py
@@ -1,10 +1,13 @@
 # app/config.py
 import os
 
+
 class Config:
     # Ustawienia trybu debug oraz klucz sesji (pobrane z zmiennych środowiskowych lub domyślne)
     DEBUG = os.environ.get('FLASK_DEBUG', False)
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'secret-key')
+    SECRET_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    SECRET_BUCKET = os.environ.get('AWS_S3_BUCKET')
+    SECRET_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
     # Konfiguracja bazy danych – używamy SQLite, ale można podmienić na np. RDS (DATABASE_URL)
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///task_manager.db')
