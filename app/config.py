48da69b402bc688f7de9aef97282d2fcaf43c954
Michał Augustyn <augustynmich@gmail.com>
2025-04-24 11:23:37 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -11,5 +11,7 @@ class Config:
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
     # Konfiguracja bazy danych – używamy SQLite, ale można podmienić na np. RDS (DATABASE_URL)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///task_manager.db')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///task_manager.db')
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:Haslo123!@database-1.cha0w2kkszs3.eu-north-1.rds.amazonaws.com:3306/taskmanagerdb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
